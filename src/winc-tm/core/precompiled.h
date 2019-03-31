980f0ad39748428bb3391e4b4907b48259940118
ropelinen <ropelinen@gmail.com>
2019-03-31 20:50:53 +0300

    Now boots both as debug and release

--- src/winc-tm/core/precompiled.h
+++ src/winc-tm/core/precompiled.h
@@ -3,6 +3,7 @@
 #pragma warning(disable : 4514) /* unreferenced inline function has been removed */
 #pragma warning(disable : 4668) /* 'xxx' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif' */
 #pragma warning(disable : 4710) /* function not inlined */
+#pragma warning(disable : 4711) /* function 'xxx' selected for automatic inline expansion */
 #pragma warning(disable : 4820) /* 'n' bytes padding added after data member 'xxx' */
 #pragma warning(disable : 5039) /* pointer or reference to potentially throwing function passed to extern C function under -EHc. Undefined behavior may occur if this function throws an exception. */ 
 #pragma warning(disable : 5045) /* Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified */
