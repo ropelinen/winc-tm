7be40283dea20bd2e44ab442091bb330c0451dc8
ropelinen <ropelinen@gmail.com>
2019-03-31 20:48:16 +0300

    Boots using a imgui dx9 example

---
+++ src/winc-tm/core/precompiled.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma warning(disable : 4514) /* unreferenced inline function has been removed */
+#pragma warning(disable : 4668) /* 'xxx' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif' */
+#pragma warning(disable : 4710) /* function not inlined */
+#pragma warning(disable : 4820) /* 'n' bytes padding added after data member 'xxx' */
+#pragma warning(disable : 5039) /* pointer or reference to potentially throwing function passed to extern C function under -EHc. Undefined behavior may occur if this function throws an exception. */ 
+#pragma warning(disable : 5045) /* Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified */
